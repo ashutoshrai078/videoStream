fdeaeb7d4cd4285acbb6e1c0c930aa5784731a84
Ashutoshrai078 <ashutoshrai0278@gmail.com>
2026-01-21 21:56:31 +0530

    utils setup

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+//all configurations
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN, //which url to be allowed
+  credentials: true
+}))
+
+app.use(express.json({limit:'16kb'})) 
+//when json is coming after form filing we've set limit to it
+
+app.use(express.urlencoded({extended:true, limit:'16kb'})) 
+// here we've put limit to data which is coming through url
+
+app.use(express.static("public")) 
+// any data which need sto be stored temporarily for that it is used eg. pdf, img,favicon etc
+
+app.use(cookieParser());
+//for performing crud ops to users browser cookies securly
+
+export {app}
