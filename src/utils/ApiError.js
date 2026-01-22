43d2ce661a11d4a98d6e5c35d3a7be54a1eb3bf3
Ashutoshrai078 <ashutoshrai0278@gmail.com>
2026-01-22 20:08:13 +0530

    utils code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error{
+  constructor (
+    statusCode,
+    message = "something wen wrong",
+    errors = [],
+    stack = ""
+  ){
+    super(message)
+    this.statusCode = statusCode,
+    this.data = null
+    this.message = message
+    this.success = false
+    this.errors = errors
+
+    if(stack){
+      this.stack= this.statck
+    }else{
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+export {ApiError}
