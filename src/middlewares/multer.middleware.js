2a380ce0d136130385616d97ded1ac3f32dfd276
Ashutoshrai078 <ashutoshrai0278@gmail.com>
2026-01-28 19:04:47 +0530

    multer setup

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,10 @@
 import multer from 'multer'
 
 const storage = multer.diskStorage({
-  destination: function (req,res,cb){
+  destination: function (req,file,cb){ // here we've told the destination where file will be stored temporarily on our server
     cb(null, './public/temp')
   },
-  filename: function(req,res,cb){
+  filename: function(req,res,cb){ // this is used to modify the filename while saving it usually we should modify it to avoid any overwrite issues but here we're saving it with original name because it will be on server for very tiny time.
     cb(null, file.originalname)
   }
 })
