99569051ac16f46ec19f9bf515e1f1a00197bfac
ashutoshrai078 <ashutoshrai0278@gmail.com>
2026-02-02 22:36:02 +0530

    bug removed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
 import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
-import ApiResponse from '../utils/ApiResponse.js'
+import {ApiResponse} from '../utils/ApiResponse.js'
 
 
 const registerUser = asyncHandler(async (req, res)=>{
@@ -57,13 +57,16 @@ const registerUser = asyncHandler(async (req, res)=>{
  const createdUser = await User.findById(user._id).select(
   '-password -refreshToken'
  )
+console.log(createdUser)
 
  if(!createdUser){
   throw new ApiError(500, 'something went wrong while registering user')
  }
 
- return res.send(201).json(new ApiResponse(200,createdUser,'User registerd Successfully'))
+  return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
 
 })
 
-export { registerUser } 
+export { registerUser }
