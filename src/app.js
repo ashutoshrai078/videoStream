a27e2d75e412ab0a9e87891cf399835a820349d4
ashutoshrai078 <ashutoshrai0278@gmail.com>
2026-02-05 22:16:25 +0530

    added routes and controllers for different feartures

--- src/app.js
+++ src/app.js
@@ -25,10 +25,28 @@ app.use(cookieParser());
 
 //Routes
 import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
 
 
 //routes declartion
 
 app.use('/api/v1/users', userRouter) // here we're sending the control to the router file standard practice when this route will be hit the control pass over
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+
 
 export {app}
